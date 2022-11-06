c5becc8385e3e77221a2bffa69d001410e9d843d
Gerf <73294402+GerfautGE@users.noreply.github.com>
2022-11-06 18:14:52 +0100

    add error generation
    errors are provided by random.randrange

---
+++ src/Error.py
@@ -0,0 +1,10 @@
+from src.FiniteField import CARDINAL, NUMBER_OF_ERRORS
+from random import randrange
+
+def error(l: list, r=CARDINAL, n=NUMBER_OF_ERRORS) -> list:
+    list_copy = l.copy()
+    TAILLE = len(list_copy)
+    for t in range(n):
+        rdm = randrange(0, TAILLE)
+        list_copy[rdm] = randrange(0, r)
+    return list_copy
